433c8adf51ece69f6b52797423351aa2a9b169c3
chenze5 <chenze5@asiainfo.com>
2025-10-08 19:17:31 +0800

    3

--- src/main/java/com/cze/helloworld/Main.java
+++ src/main/java/com/cze/helloworld/Main.java
@@ -2,6 +2,6 @@ package com.cze.helloworld;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("world");
+        System.out.println("world2");
     }
 }
